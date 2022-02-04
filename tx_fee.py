77e98af4ca5b4a0e8bf3f90f8587a464d3cfcc17
kimlong <ft.kimlong@protonmail.com>
2022-02-03 22:51:10 -0800

    Added Summary

--- tx_fee.py
+++ tx_fee.py
@@ -1,9 +1,13 @@
 #globals
+
 transaction_list = []
 header = False
 
 class Transaction:
     total_fees = 0.0
+    max_fee = 0.0
+    min_fee = float('inf')
+    num_tx = 0
 
     def __init__(self, hash, block_num, date_time, receiver, fee, method):
         self.hash = hash
@@ -14,19 +18,46 @@ class Transaction:
         self.method = method
 
         Transaction.total_fees += self.fee
+        Transaction.num_tx += 1
+
+        if self.fee < Transaction.min_fee:
+            Transaction.min_fee = self.fee
+        
+        if self.fee > Transaction.max_fee:
+            Transaction.max_fee = self.fee
     
     def display(self):
-        return f'|{self.hash:>70s}|{self.block_num:>10d}|{self.date_time:>25s}|{self.receiver:>45s}|{self.fee:>10f}|{self.method:>15s}|'
+        return f'|{self.hash:<70s}|{self.block_num:<15d}|{self.date_time:25s}|{self.receiver:45s}|{self.fee:<15f}|{self.method:20s}|'
 
-    def header(self):
-        return f'|{"Tx hash":>70s}|{"Block Number":>10d}|{"Date/Time":>25s}|{"Receiver":>45s}|{"Fees (ETH)":>10f}|{"Method":>15s}|'
-    
     @classmethod
     def total_fee(cls):
-
         return cls.total_fees
 
-#function
+    @classmethod
+    def compute_average_fees(cls):
+        if cls.num_tx > 0:
+            avg = cls.total_fees / cls.num_tx
+        else:
+            avg = None
+        
+        return avg
+    
+    @classmethod
+    def summary_string(cls):
+        avg_fees = cls.compute_average_fees()
+        output_str = cls.line(65) + f'\n|{"Total Fees":<15s}|{"Max Fees":<15s}|{"Mininum Fees":<15s}|{"Average Fees":<15s}|\n' + cls.line(65)
+        output_str += f'\n|{cls.total_fees:<15f}|{cls.max_fee:<15f}|{cls.min_fee:<15f}|{avg_fees:<15f}|\n' + cls.line(65)
+        return output_str
+
+    @staticmethod
+    def header():
+        return Transaction.line() + f'\n|{"Tx hash":<70s}|{"Block Number":<15s}|{"Date/Time":25s}|{"Receiver":45s}|{"Fees (ETH)":<15s}|{"Method":20s}|\n' + Transaction.line()
+    
+    @staticmethod
+    def line(multiplier = 197):
+        return '-' * multiplier
+
+#functions
 def load():
     with open('txinputs.csv', 'r') as infile:
         lines = infile.readlines()
@@ -55,18 +86,31 @@ def process_line(input_str):
     transaction = Transaction(hash, block_num, date_time, receiver, fee, method)
     transaction_list.append(transaction)
 
+
 def display():
     global transaction_list
+
+    if not transaction_list:
+        print('No data!')
+        return
+
+    print(Transaction.header())
     
     for transaction in transaction_list:
         print(transaction.display())
+    
+    print(Transaction.line())
 
 def summary():
-    print('Under construction')
+    global transaction_list
+    
+    if not transaction_list:
+        print('No data!')
+        return
+    
+    print(Transaction.summary_string())
 
 #main
-
-print('Total Fees spent: ', Transaction.total_fee(),'ETH')
 quit = False
 while not quit:
     print('1.Load 2.Summary 3.Display 4.Quit')
